cd43a482753d043acdd578d828cb5bae1d382cfe
Julien Rabinow <julien@jobox.ai>
2025-05-02 14:52:46 -0700

    delete dead code

--- src/main/java/com/baeldung/jdi/JDIExampleDebugger.java
+++ src/main/java/com/baeldung/jdi/JDIExampleDebugger.java
@@ -1,114 +1,41 @@
 package com.baeldung.jdi;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Map;
-import java.util.UUID;
-
-import com.sun.jdi.AbsentInformationException;
-import com.sun.jdi.Bootstrap;
-import com.sun.jdi.ClassType;
-import com.sun.jdi.IncompatibleThreadStateException;
-import com.sun.jdi.LocalVariable;
-import com.sun.jdi.Location;
-import com.sun.jdi.StackFrame;
-import com.sun.jdi.VMDisconnectedException;
-import com.sun.jdi.Value;
 import com.sun.jdi.VirtualMachine;
-import com.sun.jdi.connect.Connector;
-import com.sun.jdi.connect.IllegalConnectorArgumentsException;
-import com.sun.jdi.connect.LaunchingConnector;
-import com.sun.jdi.connect.VMStartException;
-import com.sun.jdi.event.BreakpointEvent;
-import com.sun.jdi.event.ClassPrepareEvent;
-import com.sun.jdi.event.Event;
-import com.sun.jdi.event.EventSet;
-import com.sun.jdi.event.LocatableEvent;
-import com.sun.jdi.event.StepEvent;
-import com.sun.jdi.request.BreakpointRequest;
-import com.sun.jdi.request.ClassPrepareRequest;
-import com.sun.jdi.request.StepRequest;
+import com.sun.jdi.event.*;
 
 public class JDIExampleDebugger {
-
-    private Class debugClass; 
+    private Class debugClass;
     private int[] breakPointLines;
 
-    public Class getDebugClass() {
-        return debugClass;
-    }
+    public Class getDebugClass() { return debugClass; }
+    public void setDebugClass(Class debugClass) { this.debugClass = debugClass; }
+    public int[] getBreakPointLines() { return breakPointLines; }
+    public void setBreakPointLines(int[] breakPointLines) { this.breakPointLines = breakPointLines; }
 
-    public void setDebugClass(Class debugClass) {
-        this.debugClass = debugClass;
-    }
-
-    public int[] getBreakPointLines() {
-        return breakPointLines;
-    }
-
-    public void setBreakPointLines(int[] breakPointLines) {
-        this.breakPointLines = breakPointLines;
-    }
-
-    /**
-     * Sets the debug class as the main argument in the connector and launches the VM
-     * @return VirtualMachine
-     * @throws IOException
-     * @throws IllegalConnectorArgumentsException
-     * @throws VMStartException
-     */
-    public VirtualMachine connectAndLaunchVM() throws IOException, IllegalConnectorArgumentsException, VMStartException {
-        LaunchingConnector launchingConnector = Bootstrap.virtualMachineManager().defaultConnector();
-        Map<String, Connector.Argument> arguments = launchingConnector.defaultArguments();
+    public VirtualMachine connectAndLaunchVM() throws Exception {
+        var launchingConnector = com.sun.jdi.Bootstrap.virtualMachineManager().defaultConnector();
+        var arguments = launchingConnector.defaultArguments();
         arguments.get("main").setValue(debugClass.getName());
-        VirtualMachine vm = launchingConnector.launch(arguments);
-        return vm;
+        return launchingConnector.launch(arguments);
     }
 
-    /**
-     * Creates a request to prepare the debug class, add filter as the debug class and enables it
-     * @param vm
-     */
     public void enableClassPrepareRequest(VirtualMachine vm) {
-        ClassPrepareRequest classPrepareRequest = vm.eventRequestManager().createClassPrepareRequest();
+        var classPrepareRequest = vm.eventRequestManager().createClassPrepareRequest();
         classPrepareRequest.addClassFilter(debugClass.getName());
         classPrepareRequest.enable();
     }
 
-    /**
-     * Displays the visible variables
-     * @param event
-     * @throws IncompatibleThreadStateException
-     * @throws AbsentInformationException
-     */
-    public void displayVariables(LocatableEvent event) throws IncompatibleThreadStateException, AbsentInformationException {
-        if (!event.thread().isSuspended() || event.thread().frameCount() == 0) {
-            // Skip if thread is not suspended or has no frames
-            return;
-        }
-        StackFrame stackFrame = event.thread().frame(0);
-        if(stackFrame.location().toString().contains(debugClass.getName())) {
-            Map<LocalVariable, Value> visibleVariables = stackFrame.getValues(stackFrame.visibleVariables());
-            System.out.println("Variables at " +stackFrame.location().toString() +  " > ");
-            for (Map.Entry<LocalVariable, Value> entry : visibleVariables.entrySet()) {
-                System.out.println(entry.getKey().name() + " = " + entry.getValue());
-            }
-        }
-    }
-
     public void displayVariables(JDIDebuggerCore debuggerCore) {
-        Map<String, Object> variables = debuggerCore.variables(0);
+        var variables = debuggerCore.variables(0);
         if (!variables.isEmpty()) {
             System.out.println("Variables at top frame > ");
-            for (Map.Entry<String, Object> entry : variables.entrySet()) {
+            for (var entry : variables.entrySet()) {
                 System.out.println(entry.getKey() + " = " + entry.getValue());
             }
         }
     }
 
     public static void main(String[] args) throws Exception {
-
         JDIExampleDebugger debuggerInstance = new JDIExampleDebugger();
         debuggerInstance.setDebugClass(JDIExampleDebuggee.class);
         int[] breakPoints = {6, 9};
@@ -116,54 +43,37 @@ public class JDIExampleDebugger {
         VirtualMachine vm = null;
         SessionManager sessionManager = new SessionManager();
         DebugSession session = null;
-
         try {
             vm = debuggerInstance.connectAndLaunchVM();
             debuggerInstance.enableClassPrepareRequest(vm);
             session = sessionManager.createSession(vm);
             JDIDebuggerCore debuggerCore = session.getDebuggerCore();
-
             EventSet eventSet = null;
             while ((eventSet = vm.eventQueue().remove()) != null) {
                 for (Event event : eventSet) {
                     if (event instanceof ClassPrepareEvent) {
-                        // Use JDIDebuggerCore to set breakpoints
                         String source = ((ClassPrepareEvent)event).referenceType().sourceName();
                         debuggerCore.setBreakpoints(source, debuggerInstance.getBreakPointLines());
                     }
-
                     if (event instanceof BreakpointEvent) {
                         event.request().disable();
                         debuggerInstance.displayVariables(debuggerCore);
-                        // Use JDIDebuggerCore for stepping
                         debuggerCore.stepOver(((BreakpointEvent)event).thread());
                     }
-
                     if (event instanceof StepEvent) {
                         debuggerInstance.displayVariables(debuggerCore);
                     }
                     vm.resume();
                 }
             }
-        } catch (VMDisconnectedException e) {
+        } catch (com.sun.jdi.VMDisconnectedException e) {
             System.out.println("Virtual Machine is disconnected.");
         } catch (Exception e) {
             e.printStackTrace();
-        } 
-        finally {
+        } finally {
             if (session != null) {
                 sessionManager.removeSession(session.getSessionId());
             }
-            // Move this after session cleanup
-            // InputStreamReader reader = new InputStreamReader(vm.process().getInputStream());
-            // OutputStreamWriter writer = new OutputStreamWriter(System.out);
-            // char[] buf = new char[512];
-
-            // reader.read(buf);
-            // writer.write(buf);
-            // writer.flush();
         }
-
     }
-
 }
